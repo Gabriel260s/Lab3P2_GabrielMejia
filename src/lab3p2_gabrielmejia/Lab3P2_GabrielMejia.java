a96a0d91dc06519dabb21c79bb19074882612e87
Gabriel Mejia <gmejiaservellon@gmail.com>
2024-05-03 14:02:15 -0600

    Hice el codificador

--- src/lab3p2_gabrielmejia/Lab3P2_GabrielMejia.java
+++ src/lab3p2_gabrielmejia/Lab3P2_GabrielMejia.java
@@ -4,6 +4,9 @@
  */
 package lab3p2_gabrielmejia;
 
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 /**
  *
  * @author gabri
@@ -14,7 +17,56 @@ public class Lab3P2_GabrielMejia {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println(codificar("hola"));
+        Scanner input = new Scanner(System.in);
+        System.out.println("Bienvenido\n\n1. Crear Usuario\n2.Ingresar  como empleado\n3.Ingresar como administrador\n4.Salir");
+        boolean enMenu = true;
+        while (enMenu) {
+            try {
+                System.out.println("Elija una opcion: ");
+                int opt = input.nextInt();
+                switch (opt) {
+                    case 1:
+                        System.out.println("Seleccione una opcion: \n1 .Crear Empleado\n2. Crear directivo");
+                        int optCrear = input.nextInt();
+                        switch (optCrear) {
+                            case 1:
+
+                                break;
+                            case 2:
+                                break;
+                            default:
+                                System.out.println("Esa opcion no existe.");
+                        }
+
+                        break;
+                    case 2:
+                        break;
+                    case 3:
+                        break;
+                    case 4:
+                        System.out.println("Fin del prgroma");
+                        enMenu = false;
+                        break;
+                    default:
+                        System.out.println("Esa opcion no existe.");
+                }
+
+            } catch (InputMismatchException e) {
+                System.out.println("Solo ingrese numeros validos.");
+            }
+        }
     }
-    
+
+    public static String codificar(String clave) {
+        String claveCifrada = "";
+        char letra;
+        for (int i = 0; i < clave.length(); i++) {
+            letra = (char) (int) (clave.charAt(i) + i);
+            claveCifrada += (int) letra;
+        }
+        
+        return claveCifrada;
+    }
+
 }
--- src/lab3p2_gabrielmejia/Usuarios.java
+++ src/lab3p2_gabrielmejia/Usuarios.java
@@ -4,10 +4,26 @@
  */
 package lab3p2_gabrielmejia;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.List;
+
 /**
  *
  * @author gabri
  */
 public class Usuarios {
-    
+
+    private String nombreCompleto, nombreUsuario, password;
+
+    private ArrayList<String> nombreGrupos = new ArrayList<>();
+    private HashMap<String, ArrayList> mapaMensajes = new LinkedHashMap<>();
+
+
+
+    public enum TipoDeUsuario {
+        DIRECTIVO, EMPLEADO
+    }
+
 }
